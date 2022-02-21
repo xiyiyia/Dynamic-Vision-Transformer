23425b1addb0bd534af7e9a19be956565a41a735
xiyiyia <boqianfu@gmail.com>
2022-02-21 17:53:48 +0900

    theark

--- timm/utils/metrics.py
+++ timm/utils/metrics.py
@@ -28,5 +28,7 @@ def accuracy(output, target, topk=(1,)):
     batch_size = target.size(0)
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
+
     correct = pred.eq(target.reshape(1, -1).expand_as(pred))
+    print(pred, target.reshape(1, -1).expand_as(pred), correct)
     return [correct[:k].reshape(-1).float().sum(0) * 100. / batch_size for k in topk]
