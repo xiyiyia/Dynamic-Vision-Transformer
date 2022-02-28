df48c1dfe3162be8ff45029da8331477659ca018
xiyiyia <boqianfu@gmail.com>
2022-03-01 02:19:00 +0900

    a

--- ForkTransformer.py
+++ ForkTransformer.py
@@ -207,8 +207,8 @@ def generate_logits(mlp_model, model, dataloader, T):
             output = output.max(dim=1, keepdim=False)
             print(output.values[output.values >= 8.9056])
             output.values[output.values >= 8.9056] = 0
-            print(output.values < 7.9173)
-            output.values[(output.values > 2 and output.values < 7.9173)] = 2
+            print(torch.logical_and(output.values > 2, output.values < 7.9173))
+            output.values[torch.logical_and(output.values > 2, output.values < 7.9173)] = 2
             print(output.values)
             for i in range(output.values.shape[0]):
                 if output.values[i] == 0 or output.values[i] == 2:
